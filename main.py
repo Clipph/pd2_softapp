093c285e3db8b71baa34a003f20160d7b86c7981
Clipph <kheijhackt2@gmail.com>
2025-02-13 00:07:52 +0800

    Added registration device if data doesn't exist
    The id will now be registered if ever it wasn't registered yet on the database.

--- main.py
+++ main.py
@@ -17,5 +17,11 @@ def main():
     # api = f.api_call(method="PATCH", url=f"{API_URL}/device/{id}/", data=data, api_key=API_KEY) -- this works
     # api = f.api_call(method="DELETE", url=f"{API_URL}/device/{id}/", api_key=API_KEY) -- this works
 
+    # Checks if the id is already registered. If not, it will register it
+    response = f.api_call(method="GET", url=f"{API_URL}/device/{id}/", api_key=API_KEY)
+    if response.status_code == 404:
+        response = f.api_call(method="POST", url=f"{API_URL}/register_device/", data=register_id, api_key=API_KEY)
+        print("Device is now registered")
+
 if __name__ == "__main__":
     main()
