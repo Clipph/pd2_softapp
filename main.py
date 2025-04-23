fb95cb5b913074f37e4c0a8e52cad6a6dcacb14a
Clipph <kheijhackt2@gmail.com>
2025-04-24 02:41:19 +0800

    Run process.py from venv

--- main.py
+++ main.py
@@ -2,9 +2,15 @@
 import time
 import subprocess
 import functions as f
+import os
 
 API_URL = "https://calify.pythonanywhere.com/api"
 API_KEY = "bd8e4d538dfa993960152da646"
+
+PROJECT_PATH = "/home/team46/"
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+VENV_PATH = PROJECT_PATH + "test_env/bin/python"
+PROCESS_PATH = os.path.join(CURRENT_DIR, "process.py")
 
 def main():
     id = f.get_id_from_file()
@@ -15,10 +21,10 @@ def main():
     else:
         print("The id is now registered")
 
-    while(not f.is_operating(id) and f.is_connected_to_internet()):
+    while(not f.is_operating(id) or not f.is_connected_to_internet()):
         time.sleep(5)
 
-    process = subprocess.Popen(["python", "process.py"])
+    process = subprocess.Popen([VENV_PATH, PROCESS_PATH])
 
     while(True):
         if not f.is_operating(id) or f.check_status(id) == 4:
